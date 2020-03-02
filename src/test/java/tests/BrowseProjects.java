ec5147171848df18932ef208190807e566045c23
Adam Landfrasz <adam.landfrasz@gmail.com>
2020-03-02 16:17:37 +0100

    pom

--- src/test/java/tests/BrowseProjects.java
+++ src/test/java/tests/BrowseProjects.java
@@ -1,8 +1,6 @@
 package tests;
 
-import com.sun.jdi.event.ThreadStartEvent;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
