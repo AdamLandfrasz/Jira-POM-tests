126cafd34bd3811f0e7542d01aa94cf838e118c2
Adam Landfrasz <adam.landfrasz@gmail.com>
2020-03-02 15:39:39 +0100

    pom

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,16 +2,28 @@ package tests;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class BaseTest {
     protected static final String VALID_PW = System.getenv("password");
     protected static WebDriver driver;
 
     @BeforeAll
-    protected static void setUp() {
-        driver = new ChromeDriver();
+    protected static void setUp() throws MalformedURLException {
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        capability.setBrowserName(BrowserType.CHROME);
+        capability.setPlatform(Platform.LINUX);
+        String password = System.getenv("password");
+        driver = new RemoteWebDriver(new URL("https://selenium:" + password + "@seleniumhub.codecool.codecanvas.hu/wd/hub"), capability);
         driver.manage().window().maximize();
     }
 
